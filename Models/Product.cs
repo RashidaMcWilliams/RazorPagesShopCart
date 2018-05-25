9e7af598a3a3deaec26e08c99a2a6fbdab7d4f1d
Rashida McWilliams <rashida.mcwilliams@gmail.com>
2018-05-25 11:13:17 -0400

    Initial commit

---
+++ Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RazorPagesShopCart.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
+
